4be22da43163326c3ed1c8d2029c429ce2c99fba
TNFH12 <tanianickoll11@gmail.com>
2022-02-21 20:48:01 -0500

    .

--- Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -1,10 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio6;
-
+import java.util.*;
 /**
  *
  * @author tania
@@ -15,7 +10,23 @@ public class Ejercicio6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int n = 0, m = 0; 
+       Scanner sca = new Scanner(System.in);
+       System.out.println("Digite el numero de la lista: ");
+       n = sca.nextInt();
+       int [] vector = new int [n];
+       for(int i = 0; i < vector.length; i++){
+           do{
+           vector[i] = sca.nextInt();
+           }while(vector[i]== vector[i-1]);
+        
+       }
+       for(int i = 0; i < vector.length; i++){
+           //if(vector[i]== vector[i-1]){
+            System.out.print(vector[i] + "\t"); 
+        //}
+       }
+       
     }
     
 }
